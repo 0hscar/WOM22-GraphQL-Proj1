554073f66d17983ea7f117e77da3c33a7cd05dfb
0hscar <oscar.weber00@gmail.com>
2022-10-19 03:25:58 +0300

    Resent testing

--- server.js
+++ server.js
@@ -1,20 +1,43 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
+var fetch = require("node-fetch").default
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
 
+var schema2 = buildSchema(`
+    type Query {
+        getMessage(content: String): Message
+    }
+
+    type Message {
+        content: String
+    }
+`)
+
+
+
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+  rollDice: ({numDice, numSides}) => {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  }
 };
+
+var root2 = {
+    content: () =>{
+        return 'Tjongerbonger'
+    }
+}
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
@@ -22,5 +45,35 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root,
   graphiql: true,
 }));
+
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema2,
+    rootValue: root2,
+    graphiql: true,
+}))
+
+
 app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+
+var dice = 3;
+var sides = 6;
+var query = `query RollDice($dice: Int!, $sides: Int) {
+  rollDice(numDice: $dice, numSides: $sides)
+}`;
+
+fetch('http://localhost:4000/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    variables: { dice, sides },
+  })
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));
